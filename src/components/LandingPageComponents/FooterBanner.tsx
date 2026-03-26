f2aa718bb3ef0e308f20800485c5c9d7be316e26
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-03-26 12:25:15 +0600

    Add FooterBanner component with image display

---
+++ src/components/LandingPageComponents/FooterBanner.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import Image from "next/image";
+
+export default function FooterBanner() {
+  return (
+    <section className="w-full bg-[#f3f3f3] px-0">
+      <div className="relative mx-auto h-30 w-full  overflow-hidden">
+        <Image
+          src="/footerBanner.jpg"
+          alt="Footer decorative banner"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </section>
+  );
+}
